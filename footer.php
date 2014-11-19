4940e91e9e23c5b8386127802b748fb022efff22
Joel Rathgaber <joel.rathgaber@gmail.com>
2014-11-18 22:11:12 -0600

    added favicons

--- footer.php
+++ footer.php
@@ -10,10 +10,10 @@
     					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
     					'theme_location' => 'footer-links',             // where it's located in the theme
     					'before' => '',                                 // before the menu
-						'after' => '',                                  // after the menu
-						'link_before' => '',                            // before each link
-						'link_after' => '',                             // after each link
-						'depth' => 0,                                   // limit the depth of the nav
+						  'after' => '',                                  // after the menu
+						  'link_before' => '',                            // before each link
+						  'link_after' => '',                             // after each link
+						  'depth' => 0,                                   // limit the depth of the nav
     					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
 						)); ?>
 					</nav>
